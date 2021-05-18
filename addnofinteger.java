e2bb182ae85c90d7f5705220f4c9ccc916192f9d
somanathbaravkar <84221741+somanathbaravkar@users.noreply.github.com>
2021-05-18 08:52:20 +0530

    Update addnofinteger.java
    i have changed integer values

--- addnofinteger.java
+++ addnofinteger.java
@@ -2,9 +2,9 @@ class addnofinteger
 {
 public static void main(String args[])
 {
-addition(10,20);
+addition(20,20);
 addition(5,6);
-addition(50,50);
+addition(50,20);
 }
 static void addition(int num1,int num2)
 {
