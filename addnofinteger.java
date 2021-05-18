5f5bccaf338ff2a167ebacc13ba595a8d8c4bcfa
somanathbaravkar <84221741+somanathbaravkar@users.noreply.github.com>
2021-05-18 08:48:00 +0530

    first commit

---
+++ addnofinteger.java
@@ -0,0 +1,14 @@
+class addnofinteger
+{
+public static void main(String args[])
+{
+addition(10,20);
+addition(5,6);
+addition(50,50);
+}
+static void addition(int num1,int num2)
+{
+int sum=num1+num2;
+System.out.println(sum);
+}
+}
